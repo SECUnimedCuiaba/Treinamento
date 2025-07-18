25a3660a68809c33bbaf9d4cb3bf9e115af41b4d
SECUnimedCuiaba <sec.unimedcuiaba@equipacare.com.br>
2025-07-18 14:31:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -281,7 +281,7 @@ const equipmentData = {
        "cardioversorPhilips", "monitorPhilips", "ecgAlfamed",  "ventiladorResmed",  "camaArjo", "termohigrometro","camaraFanem"
       ],
       "pa": [
-        "ecgAlfamed", "cardioversorPhilips", "cardioversorInstramed8", "cardioversorApolus","ventiladorTecme", "ventiladorResmed"
+        "ecgAlfamed", "cardioversorPhilips", "cardioversorInstramed8", "monitorDragerVista120S", "cardioversorApolus","ventiladorTecme", "ventiladorResmed"
       ],
       "farmacia": [
         "estufaFanem", "termohigrometro", "camaraFanem","camaraIndrel","camaraBiotecno"
